6d56df94ef78467b6553c58ea07cd31af1a93642
zhouhanchao <18729808260@163.com>
2017-08-28 17:47:49 +0800

    初始列为单机版。逐渐修改

--- pages/house/index.js
+++ pages/house/index.js
@@ -1,10 +1,8 @@
-var openid = "12312312312312";
 var game = {
-    personNum: 5 //游戏有几个人
+    personNum: 8 //游戏有几个人
     , banker: 0 //庄家
-    , totalGame: 2
+    , totalGame: 8
     , nowGame: 1
-    , isHidden: "hidden"
     , timing: 3 // 初始倒计时
         
     , transform4: [{ // 4人时位置
@@ -60,7 +58,6 @@ var game = {
                                 <img src="../../image/card/behind.png">\
                             </div>';
             transform = personNum <= 4 ? _this.transform4 : _this.transform8;
-            
             $('#cardBehind').append(cardHtml);
             $('#cardBehind .g' + i + '').animate(transform[i], "fast", function () {
                 argu.callee.call(_this, personNum, ++i, ++index); //递归调用，绑定this
@@ -114,9 +111,7 @@ var game = {
     , countNum: function (personNum) { // 一局所得分数
         var scoreHtml = '';
         var transform = null;
-        
-        transform = personNum <= 4 ? _this.transform4 : _this.transform8;
-        
+        transform = personNum <= 4 ? this.transform4 : this.transform8;
         for (let i = 0; i < personNum; i++) {
             scoreHtml += '<div class="hidden">+' + (Math.floor(Math.random() * 100)) + '</div>';
         }
@@ -130,23 +125,24 @@ var game = {
         this.clearTable();
     }
     , clearTable: function () { // 重新开始游戏
-        var _this = this
-            , betHtml = '<div class="btns">\
-                            <button>申请上庄</button>\
-                            <button class="ready">准备</button>\
-                        </div>';
+        var _this = this;
+        var btn = this.nowGame < this.totalGame ? "准备" : "查看排行榜";
+        var betHtml = '<div class="btns">\
+                        <!--<button>申请上庄</button>-->\
+                        <button class="ready">'+btn+'</button>\
+                    </div>';
         $('#table').append(betHtml);
         $(document).one('click', '#table .btns .ready', function () { // one确保点击只执行一次
             ++_this.nowGame;
             if (_this.nowGame <= _this.totalGame) {
                 $(this).parent().remove();
                 $('#cardBehind').html('<div class="group base"> <img src="../../image/card/behind.png"> <img src="../../image/card/behind.png"> </div>').hide();
-//                $('#man .person1>div').html('<img src="../../image/ready.png" class="block readyBtn r1">');
+                $('#man div img.r1').show();
                 $('#scoreNum').html('');
                 _this.init();
             }
             else {
-                alert('Game Over');
+                location.href = "../ranking/ranking.html";
             }
         });
     }
@@ -159,7 +155,7 @@ var game = {
                 clearInterval(interval);
                 interval = null;
                 $("#table #timing").remove(); //删除倒计时
-                $('#table #man .readyBtn').parent().html(''); //清空准备状态
+                $('#table #man .readyBtn').hide(); //清空准备状态
                 if (state && state == 1) { //准备完毕
                     _this.cardDeal(_this.personNum, _this.banker, 0); //调用发牌函数;
                 }
@@ -174,32 +170,31 @@ var game = {
             }
         }, 1000);
     }
-    , forPerson: function (personNum,data) { // 每个人的位置
-        var num = data;
-        var personHtml = "";
-        
-        num = this.findOpenId(num);
-        for (let i = 0; i < num.length; i++) {
-            var j = i + 1;
-            personHtml += '<div class="person person' + j + '">\
-                                <p class="avatar"><img data-openid="'+num[i].openid+'" src="'+num[i].avatar+'" alt=""></p>\
-                                <p class="name">'+num[i].name+'</p>\
-                                <p class="gold"><img src="../../image/gold.png" alt=""><span>'+num[i].gold+'</span></p>';
-            if (i == this.banker) {
-                personHtml += '<span class="banker">庄</span>';
-            }
-            if (num[i].state == 1) {
-                personHtml += '<div><img src="../../image/ready.png" class="block readyBtn r' + j + '"></div>';
-            }else {
+    , forPerson: function (personNum, data) { // 每个人的位置
+        if (this.nowGame <= 1) {
+            var personHtml = "";
+            for (let i = 0; i < data.length; i++) {
+                var j = i + 1;
+                personHtml += '<div class="person person' + j + '">\
+                                    <p class="avatar"><img src="' + data[i].avatar + '" alt=""></p>\
+                                    <p class="name">' + data[i].name + '</p>\
+                                    <p class="gold"><img src="../../image/gold.png" alt=""><span>' + data[i].gold + '</span></p>';
+                
                 personHtml += '<div><img src="../../image/ready.png" class="hidden readyBtn r' + j + '"></div>';
+                personHtml += '</div>';
             }
-            personHtml += '</div>';
+            $('#table #man').html(personHtml);
         }
-        $('#table #man').html(personHtml);
+        console.log(this.banker);
+        $('#table #man div.person').map(function(i,item){
+            $(item).find('.banker').remove();
+            
+        });
+        $('#table #man div.person').eq(this.banker).append('<span class="banker">庄</span>');
         $('#table #man div>img.readyBtn').map(function (i, item) { //随机时间模拟准备状态
             setTimeout(function () {
-                if (!$(item).hasClass('r1')) {
-                    $(item).show(); //r1视为自己
+                if (!$(item).hasClass('r1')) { //r1视为自己
+                    $(item).show(); 
                 }
             }, Math.random() * 4000 + 1000);
         });
@@ -211,45 +206,59 @@ var game = {
             this.personIndex();
         }
     }
-    , findOpenId: function(data){ // openid将自己放在第一位
-        var Own = null;
-        if(data && data.length){
-            for(let i=0;i<data.length;i++){
-                if(openid.indexOf(data[i].openid) != -1){
-                   Own = data.splice(i,1)[0];
-                }
-            }
-        }
-        if(Own){
-            data.unshift(Own);
-        }
-        return data;
-    }
-    , falseData: function(personNum){
-        
+    , falseData: function (personNum) {
         var _this = this;
-        var one = [{id: 3,state: '1',openid:'213123123123123',name:'Dome',gold:'56',avatar:'../../image/eg_avatar.png'},{id: 4,state: '1',name:'Fril',gold:'76',openid:'213123123123123',avatar:'../../image/eg_avatar.png'}]
         var data = [
-            {id: 1,state: '1',openid:'123456789',name:'Tom2',gold:'33',avatar:'../../image/eg_avatar.png'},
-            {id: 2,state: '0',openid:'12312312312312',name:'Mark',gold:'12',avatar:'../../image/eg_avatar.png'},
-            {id: 1,state: '1',openid:'123456789',name:'Tom3',gold:'33',avatar:'../../image/eg_avatar.png'},
-            {id: 1,state: '1',openid:'123456789',name:'Tom4',gold:'33',avatar:'../../image/eg_avatar.png'},
-            {id: 1,state: '1',openid:'123456789',name:'Tom5',gold:'33',avatar:'../../image/eg_avatar.png'},
-//            {id: 1,state: '1',openid:'123456789',name:'Tom6',gold:'33',avatar:'../../image/eg_avatar.png'},
-//            {id: 1,state: '1',openid:'123456789',name:'Tom7',gold:'33',avatar:'../../image/eg_avatar.png'},
-//            {id: 1,state: '1',openid:'123456789',name:'Tom8',gold:'33',avatar:'../../image/eg_avatar.png'},
-             
-        ];
-        
-        if(this.nowGame>1){ // 模拟第二把之后的自己准备状态，state==1
-            data[1].state = 1;
-        }
-//        setTimeout(function(){
-//            data = data.concat(one);
-//            _this.forPerson(personNum,data);
-//        },3000);
-        
-        this.forPerson(personNum,data);
+            {
+                id: 1
+                , name: 'ZHOU'
+                , gold: '33'
+                , avatar: '../../image/eg_avatar4.png'
+            }
+            , {
+                id: 2
+                , name: 'Mark'
+                , gold: '12'
+                , avatar: '../../image/eg_avatar5.png'
+            }
+            , {
+                id: 1
+                , name: 'Tom3'
+                , gold: '33'
+                , avatar: '../../image/eg_avatar2.png'
+            }
+            , {
+                id: 1
+                , name: 'Tom4'
+                , gold: '33'
+                , avatar: '../../image/eg_avatar3.png'
+            }
+            , {
+                id: 1
+                , name: 'Tom5'
+                , gold: '33'
+                , avatar: '../../image/eg_avatar5.png'
+            }
+            , {
+                id: 1
+                , name: 'Tom6'
+                , gold: '33'
+                , avatar: '../../image/eg_avatar2.png'
+            }
+            , {
+                id: 1
+                , name: 'Tom7'
+                , gold: '33'
+                , avatar: '../../image/eg_avatar.png'
+            }
+            , {
+                id: 1
+                , name: 'Tom8'
+                , gold: '33'
+                , avatar: '../../image/eg_avatar.png'
+            }
+        , ];
+        this.forPerson(personNum, data);
     }
     , isAllReady: function (personNum) { // 检测每个人是否准备好
         var _this = this;
